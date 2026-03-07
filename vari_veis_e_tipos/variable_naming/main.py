88f7fde49e6a8bba6da8a093973887f6db9f2768
felipeschawsnekker <>
2026-03-07 17:19:56 +0000

    Variable Naming

---
+++ vari_veis_e_tipos/variable_naming/main.py
@@ -0,0 +1,10 @@
+item="Bread"
+item_price=float(4.52)
+items_in_stock=int(230)
+
+
+
+# Testing
+print("Item:", item)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", items_in_stock)
