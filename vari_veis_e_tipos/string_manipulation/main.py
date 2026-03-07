af4f01a6edb0f1c80f2da39c456fdeaf296819d0
felipeschawsnekker <>
2026-03-07 17:36:43 +0000

    String Manipulation

---
+++ vari_veis_e_tipos/string_manipulation/main.py
@@ -0,0 +1,7 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1=grocery_items[0:5]
+dairy2=grocery_items[5:12]
+bakery1=grocery_items[12:17]
+total=dairy1+dairy2+bakery1
+
+print(f"We have dairy and bakery: {total} in aisle 5")
